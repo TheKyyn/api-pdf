ce116a05fadb6d6273e1ba7806c2d776418a8eeb
Kyyn <maxtheo155@icloud.com>
2024-02-16 17:44:30 +0100

    initialized pdfkit library to generate very basic pdf files

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
 
 app.get('/', (req, res) => {
     res.send('Weeee saluuuut');
@@ -8,4 +10,18 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
+
+app.get('/generate-pdf', (req, res) => {
+    const doc = new PDFDocument();
+
+    let filename = 'Document';
+    filename = encodeURIComponent(filename) + '.pdf';
+    res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"');
+    res.setHeader('Content-type', 'application/pdf');
+
+    doc.text('Saluuuut, ceci est un exemple de PDF test pour savoir si tout marche bien hihi', 100, 100);
+
+    doc.pipe(res);
+    doc.end();
 });
